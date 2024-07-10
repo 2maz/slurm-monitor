016c644d6cd5c268d50825c8e7a457462e26c866
Thomas Roehr <roehr@simula.no>
2024-07-10 17:17:07 +0200

    Draft access to gpu status

---
+++ src/slurm_monitor/app_settings.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from pydantic import Field
+from pydantic_settings import BaseSettings
+from slurm_monitor.db.db import DatabaseSettings
+
+class AppSettings(BaseSettings):
+    database: DatabaseSettings = Field(default_factory=DatabaseSettings)
+
+    @classmethod
+    def get_instance(cls) -> AppSettings:
+        if not cls._instance:
+            raise RuntimeError("AppSettings: instance is not accessible. Please call AppSettings.initialize() first.")
+
+        return cls._instance
+
+    @classmethod
+    def initialize(cls) -> None:
+        cls._instance = AppSettings()
+        cls._instance.database.uri = "sqlite:////tmp/test.sqlite"
---
+++ src/slurm_monitor/db_operations.py
@@ -0,0 +1,22 @@
+import functools
+import io
+from logging import getLogger, Logger
+import sqlalchemy
+
+from fastapi import HTTPException
+from slurm_monitor.app_settings import AppSettings
+from slurm_monitor.db.db import SlurmMonitorDB
+
+logger : Logger = getLogger(__name__)
+
+@functools.cache
+def get_database(app_settings: AppSettings | None = None):
+    if app_settings is None:
+        app_settings = AppSettings.get_instance()
+
+    logger.info("Loading database with: {app_settings.database}")
+    try:
+        return SlurmMonitorDB(app_settings.database)
+    except sqlalchemy.exc.OperationalError:
+        raise HTTPException(status_code=500, detail=f"Cannot access monitor database - {app_settings.database.uri}")
+
