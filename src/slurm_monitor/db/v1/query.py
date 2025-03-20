69a3c0a5ec0abc36f0fb7af58ce96356026afa0f
Thomas Roehr <roehr@simula.no>
2025-03-20 21:48:15 +0100

    query: add sqlite option

--- src/slurm_monitor/db/v1/query.py
+++ src/slurm_monitor/db/v1/query.py
@@ -21,7 +21,12 @@ class Query:
             return pd.DataFrame(result.fetchall(), columns=result.keys())
 
     def execute(self) -> pd.DataFrame:
-        return self._execute(text(self.statement), {})
+        statement = self.statement
+        if self._db.db_url.get_dialect().name == "sqlite":
+            if hasattr(self, "statement_sqlite"):
+                statement = self.statement_sqlite
+
+        return self._execute(text(statement), {})
 
     async def _execute_async(self, query: str, params: dict[str, any] = {}):
         async with self._db.make_async_session() as session:
@@ -57,6 +62,28 @@ class UserJobResults(Query):
         FROM job_status
         WHERE
             job_state in ('COMPLETED','CANCELLED','FAILED')
+        GROUP BY user_id
+        ORDER BY number_of_jobs;
+    """
+
+    statement_sqlite: str = """
+        SELECT 
+          ROW_NUMBER() OVER (ORDER BY user_id) AS anon_user_id, user_id,
+              (
+                COUNT
+                    (CASE WHEN 
+                        exit_code = 0 AND job_state IN ('COMPLETED', 'FAILED', 'CANCELLED')
+                        THEN 1 END) * 100.0 / COUNT(*)
+              ) AS share_of_successful_jobs,
+              COUNT(DISTINCT job_id) AS number_of_jobs,
+              AVG(STRFTIME('%s', end_time) - STRFTIME('%s', start_time)) AS avg_time,
+              MIN(STRFTIME('%s', end_time) - STRFTIME('%s', start_time)) AS min_time,
+              MAX(STRFTIME('%s', end_time) - STRFTIME('%s', start_time)) AS max_time,
+              AVG(cpus) AS avg_cpus,
+              AVG(node_count) AS avg_node_count,
+              AVG(tasks) AS avg_tasks
+        FROM job_status
+        WHERE job_state IN ('COMPLETED', 'CANCELLED', 'FAILED')
         GROUP BY user_id
         ORDER BY number_of_jobs;
     """
