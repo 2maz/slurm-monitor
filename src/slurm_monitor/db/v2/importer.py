5200b583b1cd6b89da80875b96eb8bfc08b7c82a
Thomas Roehr <roehr@simula.no>
2026-02-26 09:54:29 +0100

    sonar: add expansion of HostnameRange

--- src/slurm_monitor/db/v2/importer.py
+++ src/slurm_monitor/db/v2/importer.py
@@ -24,7 +24,6 @@ from slurm_monitor.db.v2.db_tables import (
     TableBase,
 )
 from slurm_monitor.db.v2.db import ClusterDB
-
 import slurm_monitor.db.v2.sonar as sonar
 
 logger = logging.getLogger(__name__)
@@ -325,7 +324,7 @@ class DBJsonImporter(Importer):
         nodes_states = []
         for n in attributes["nodes"]:
             for names in n["names"]:
-                node_names = Slurm.expand_node_names(names)
+                node_names = sonar.Sonar.expand_hostname_range(names)
                 nodes.update(node_names)
 
             states = n["states"]
@@ -340,7 +339,7 @@ class DBJsonImporter(Importer):
         for p in attributes["partitions"]:
             partition_nodes = set()
             for names in p["nodes"]:
-                node_names = Slurm.expand_node_names(names)
+                node_names = sonar.Sonar.expand_hostname_range(names)
                 partition_nodes.update(node_names)
 
             partitions.append(
@@ -382,6 +381,9 @@ class DBJsonImporter(Importer):
             if "sacct" in job_data:
                 sacct = job_data["sacct"]
                 del job_data["sacct"]
+
+            if 'nodes' in job_data:
+                job_data['nodes'] = sonar.Sonar.expand_hostname_range(job_data['nodes'])
 
             slurm_job_samples.append(
                 SampleSlurmJob.create(cluster=cluster, **job_data, time=time)
--- src/slurm_monitor/db/v2/sonar.py
+++ src/slurm_monitor/db/v2/sonar.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from slurm_monitor.db.v2.db_tables import ErrorMessage
 import dataclasses
 from enum import Enum
-
+import re
 
 @dataclasses.dataclass
 class Meta:
@@ -40,3 +40,64 @@ class TopicType(str, Enum):
         raise ValueError(
             "TopicType.infer: '{topic}' does not have a value topic type suffix"
         )
+
+class Sonar:
+    @classmethod
+    def expand_simple_range(cls, hostname_range: str) -> list[str]:
+        cls.validate_simple_range(hostname_range)
+
+        expanded = []
+        sections = hostname_range[1:-1].split(",")
+        for section in sections:
+            range_expr = section.split("-")
+            if len(range_expr) == 1:
+                expanded.append(int(range_expr[0]))
+            elif len(range_expr) == 2:
+                expanded += range(int(range_expr[0]), int(range_expr[1])+1)
+            else:
+                raise ValueError(f"Invalid pattern encountered for range: {range_expr}")
+
+        return expanded
+
+    @classmethod
+    def validate_simple_range(cls, hostname_range: str):
+        m = re.match(r"\[[\d,-]+\]", hostname_range)
+        if not m:
+            raise ValueError(f"Given expression: '{hostname_range}' is not a hostname range")
+
+
+    @classmethod
+    def expand_hostname_range(cls, hostname_range: str):
+        """
+        Expand hostname range so that processing `c[1-3,5]-[2-4].fox` yields:
+            `c1-2.fox`, `c1-3.fox`, `c1-4.fox`, `c2-2.fox`, `c2-3.fox`, `c2-4.fox`, `c3-2.fox`, `c3-3.fox`, `c3-4.fox`, `c5-2.fox`, ...
+        """
+
+        # split at comma that are not within brackets
+        ranges = re.split(r',\s*(?![^\[]*\])', hostname_range)
+
+        nodes = []
+        for hostnames_expr in ranges:
+            includes_ranges = re.findall(r'(?P<pre>[^ \[\]]*)(?P<range>\[[\d,-]+\])(?P<post>[^ \[\]]*)', hostnames_expr)
+            if not includes_ranges:
+                # single node name
+                nodes.append(hostnames_expr.strip())
+
+            # Now we need to iterate over all ranges
+            # a single match is now a triple (pre, range, post), where pre and post an constant elements
+            # we will create a set of partical completions that will be continously extended base on the encountered ranges
+            nodenames = []
+            for match_group in includes_ranges:
+                pre, hostname_range, post = match_group
+                if nodenames:
+                    nodenames = [x+pre for x in nodenames]
+                else:
+                    nodenames = [pre]
+
+                expanded = cls.expand_simple_range(hostname_range)
+                nodenames = [n+str(e) for n in nodenames for e in expanded]
+                nodenames = [x+post for x in nodenames]
+
+            nodes += nodenames
+
+        return nodes
