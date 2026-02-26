9a660c624cc35c5d2f608f9a11dd82f887932c11
Thomas Roehr <roehr@simula.no>
2026-02-26 15:55:32 +0100

    db: fix diskstat rate computation

--- src/slurm_monitor/db/v2/db.py
+++ src/slurm_monitor/db/v2/db.py
@@ -2156,17 +2156,19 @@ class ClusterDB(Database):
             all_disks = (await session.execute(query)).mappings().all()
 
         disks = []
+        disks_ignored = []
         for d in all_disks:
             disk_name = d["name"]
             if disk_name.startswith("loop"):
-                logger.info(f"Ignoring disk stats for loop device: {disk_name}")
+                disks_ignored.append(disk_name)
                 continue
             elif re.search(r"p[0-9]+$", disk_name):
-                logger.info(f"Ignoring diskstats for partition: {disk_name}")
+                disks_ignored.append(disk_name)
                 continue
 
             disks.append(d)
 
+        logger.info(f"Ignoring disk stats for {disks_ignored}")
         disks = sorted(disks, key=lambda x: x["name"])
 
         response = []
@@ -2183,7 +2185,7 @@ class ClusterDB(Database):
         for disk in tqdm(disks, desc="Disks", total=len(disks)):
             query = (
                 select(
-                    time_bucket(resolution_in_s, SampleDisk.time).label("time"),
+                    time_bucket(resolution_in_s, SampleDisk.time).label("bucket_time"),
                     *fields,
                 )
                 .where(
@@ -2193,14 +2195,14 @@ class ClusterDB(Database):
                     (SampleDisk.time >= fromtimestamp(start_time_in_s)),
                     (SampleDisk.time <= fromtimestamp(end_time_in_s)),
                 )
-                .group_by("time")
-                .order_by("time")
+                .group_by("bucket_time")
+                .order_by("bucket_time")
             )
 
             async with self.make_async_session() as session:
                 result = (await session.execute(query)).mappings().all()
                 data = [
-                    SampleDiskResponse(**x, delta_time_in_s=resolution_in_s)
+                    SampleDiskResponse(**x, time=x["bucket_time"], delta_time_in_s=resolution_in_s)
                     for x in result
                 ]
 
