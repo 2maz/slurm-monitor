022626038c63dad992148abb81232cc29e2025d1
Thomas Roehr <roehr@simula.no>
2026-03-06 14:35:30 +0100

    ci: add container generation

---
+++ docker/prepare-docker.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+#
+apt update
+apt upgrade -y
+
+export DEBIAN_FRONTEND=noninteractive
+apt install -y \
+    curl \
+    git \
+    locales \
+    openssl \
+    python3 \
+    sudo \
+    tzdata \
+    vim \
+    wget
+
+echo "Europe/Oslo" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+export LANGUAGE=en_EN.UTF-8
+export LANG=en_EN.UTF-8
+export LC_ALL=en_EN.UTF-8
+
+locale-gen en_EN.UTF-8
+dpkg-reconfigure locales
+
+useradd -ms /bin/bash docker
+echo "docker ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+chown -R docker:docker /home/docker
+
+git config --global user.email "roehr@simula.no"
+git config --global user.name "Thomas Roehr"
