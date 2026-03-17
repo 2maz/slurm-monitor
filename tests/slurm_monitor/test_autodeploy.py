05e2703b73e2ea9299b6ceb8e6bf7622ca901ef8
Thomas Roehr <roehr@simula.no>
2026-03-17 10:59:18 +0100

    Fix autodeploy setup

--- tests/slurm_monitor/test_autodeploy.py
+++ tests/slurm_monitor/test_autodeploy.py
@@ -3,15 +3,18 @@ from slurm_monitor.app_settings import AppSettings
 from slurm_monitor.db import DatabaseSettings
 import time
 
-def test_AutoDeployer(test_db, test_db_uri, number_of_nodes, monkeypatch):
+def test_AutoDeployer_v1(test_db, test_db_uri, number_of_nodes, monkeypatch):
+    redeploy_nodes = set()
 
-    redeploy_nodes = []
     def mock_deploy(self, node):
-        redeploy_nodes.append(node)
+        redeploy_nodes.add(node)
 
     # exclude drained nodes from redeployment
     async def mock_is_drained(self, node):
         return True if node == "node-0" else False
+
+    def mock_all_nodes(self) -> list[str]:
+        return [f"node-{x}" for x in range(0, number_of_nodes)]
 
     app_settings = AppSettings()
     app_settings.db_schema_version = "v1"
@@ -21,6 +24,7 @@ def test_AutoDeployer(test_db, test_db_uri, number_of_nodes, monkeypatch):
 
     monkeypatch.setattr(AutoDeployer, "deploy", mock_deploy)
     monkeypatch.setattr(AutoDeployer, "is_drained", mock_is_drained)
+    monkeypatch.setattr(AutoDeployer, "all_nodes", mock_all_nodes)
 
     auto_deployer = AutoDeployer(app_settings=app_settings, sampling_interval_in_s=1)
     auto_deployer.start()
@@ -31,14 +35,18 @@ def test_AutoDeployer(test_db, test_db_uri, number_of_nodes, monkeypatch):
     assert len(set(redeploy_nodes)) == number_of_nodes - 1
     assert "node-0" not in redeploy_nodes
 
-def test_AutoDeployerSonar(timescaledb, test_db_v2, db_config, monkeypatch):
-    redeploy_nodes = []
+def test_AutoDeployer_v2(timescaledb, test_db_v2, db_config, monkeypatch):
+    redeploy_nodes = set()
+
     def mock_deploy(self, node):
-        redeploy_nodes.append(node)
+        redeploy_nodes.add(node)
 
     # exclude drained nodes from redeployment
     async def mock_is_drained(self, node):
         return True if node == "cluster-0-node-0" else False
+
+    def mock_all_nodes(self) -> list[str]:
+        return [f"cluster-0-node-{x}" for x in range(0, db_config.number_of_nodes)]
 
     app_settings = AppSettings()
     app_settings.db_schema_version = "v2"
@@ -47,7 +55,8 @@ def test_AutoDeployerSonar(timescaledb, test_db_v2, db_config, monkeypatch):
     )
 
     monkeypatch.setattr(AutoDeployerSonar, "deploy", mock_deploy)
-    monkeypatch.setattr(AutoDeployerSonar, "is_drained",mock_is_drained)
+    monkeypatch.setattr(AutoDeployerSonar, "is_drained", mock_is_drained)
+    monkeypatch.setattr(AutoDeployerSonar, "all_nodes", mock_all_nodes)
 
     auto_deployer = AutoDeployerSonar(
             app_settings=app_settings,
@@ -58,5 +67,5 @@ def test_AutoDeployerSonar(timescaledb, test_db_v2, db_config, monkeypatch):
     time.sleep(3)
     auto_deployer.stop()
 
-    assert len(set(redeploy_nodes)) == db_config.number_of_nodes - 1
+    assert len(redeploy_nodes) == db_config.number_of_nodes - 1, f"Trying to redeploy {redeploy_nodes}, but expected only {db_config.number_of_nodes - 1} nodes, due to one drained node"
     assert "cluster-0-node-0" not in redeploy_nodes
