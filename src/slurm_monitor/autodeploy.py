57e93f21e1333dc97c07456f7b056a5574bf641e
Thomas Roehr <roehr@simula.no>
2026-03-27 16:16:58 +0100

    cli: update autodeploy

--- src/slurm_monitor/autodeploy.py
+++ src/slurm_monitor/autodeploy.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
 import asyncio
+import curses
 from threading import Thread
 import time
+import traceback as tb
 import datetime as dt
 import logging
 import json
-import os
 from pathlib import Path
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
+import sys
 
 from slurm_monitor.db_operations import DBManager
 from slurm_monitor.app_settings import AppSettings
@@ -29,6 +31,7 @@ class AutoDeployerStats(BaseSettings):
 
 class AutoDeployer:
     thread: Thread
+    _last_update: dt.datetime | None = None
     _stop: bool = False
     _sampling_interval_in_s: float
 
@@ -39,6 +42,8 @@ class AutoDeployer:
     app_settings: AppSettings
     cluster_name: str
     deploy_command: str
+
+    _getch_supported: bool
 
     def __init__(self,
             app_settings: AppSettings | None = None,
@@ -60,6 +65,12 @@ class AutoDeployer:
         self.cluster_name = cluster_name
         self.deploy_command = deploy_command
         self.allow_list = allow_list
+
+        self.messages = []
+
+        self._screen = None
+        self._getch_supported = True
+
 
     def start(self):
         self._stop = False
@@ -93,71 +104,140 @@ class AutoDeployer:
         with open(Path(filename), "w") as f:
             json.dump(self.stats.model_dump(), f, indent=4, default=str)
 
+    def addstr(self, y, x, text, attr = None):
+        screenheight, screenwidth = self._screen.getmaxyx()
+        if y >= screenheight:
+            return
+
+        writeable_x = screenwidth - x -1
+        if writeable_x < 1:
+            return
+
+        if attr:
+            self._screen.addstr(y, x, text[:writeable_x], attr)
+        else:
+            self._screen.addstr(y, x, text[:writeable_x])
+
+    def show(self):
+        try:
+            if not self._screen:
+                self._screen = curses.initscr()
+                self._screen.clear()
+                curses.noecho()
+                try:
+                    curses.cbreak()
+                except Exception:
+                    logger.warning("Terminal does not support 'cbreak' - key interactions will be disabled")
+                    self._getch_supported = False
+
+                self._screen.nodelay(True)
+
+            self._screen.erase()
+
+
+            elapsed = (utcnow() - self._last_update).total_seconds()
+            delta_in_s = self._sampling_interval_in_s - elapsed
+
+            # header
+            screenheight, screenwidth = self._screen.getmaxyx()
+            current_time = f"-- CURRENT TIME  {utcnow().isoformat(timespec='seconds')} | NEXT UPDATE IN {int(delta_in_s)}s"
+            self.addstr(0, 0, f"{current_time}{'-'*(screenwidth-len(current_time))}")
+            self.addstr(1, 0, f">> Status: slurm-monitor auto-deploy --cluster-name {self.cluster_name}")
+            self.addstr(2, 0, "   q to quit ")
+            self.addstr(3, 0, " "*screenwidth)
+
+            for idx, msg in enumerate(self.messages):
+                self.addstr(idx + 4, 0, msg)
+
+            if self._getch_supported:
+                key = self._screen.getch()
+                if key == ord('q'):
+                    self._stop = True
+                    self.addstr(0,0, "Received user's request to stop ... ]")
+
+
+            self._screen.refresh()
+        except Exception as e:
+            logger.error(f"Screen update failed: {e}")
+            print(f"Screen update failed: {e}")
+            tb.print_tb(e.__traceback__)
+            sys.exit(0)
+
     def run(self):
-        start_time = None
+        self._last_update = None
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
 
-        while not self._stop:
-            now = utcnow()
-            if start_time:
-                elapsed = (now - start_time).total_seconds()
-                if elapsed < self._sampling_interval_in_s:
-                    time.sleep(self._sampling_interval_in_s - elapsed)
-            else:
-                start_time = utcnow()
-
-            now = utcnow()
-            os.system("clear")
-            print(f"-- autodeploy check: {now}")
-
-            last_probe_timestamp = None
-            if self.app_settings.db_schema_version == "v1":
-                last_probe_timestamp = loop.run_until_complete(self.dbi.get_last_probe_timestamp())
-            else:
-                if self.cluster_name is None:
-                    raise ValueError("Missing cluster_name")
-
-                last_probe_timestamp = loop.run_until_complete(
-                        self.dbi.get_last_probe_timestamp(cluster=self.cluster_name)
-                )
-                logger.info(last_probe_timestamp)
-
-            expected_nodes = self.all_nodes()
-            for node in expected_nodes:
-                node_time = last_probe_timestamp.get(node, None)
-                if not node_time:
-                    msg = f"{node} has not been seen before"
-                    if self.allow_list is None or node in self.allow_list:
-                        msg += ", but is in the allow list"
-                        if not loop.run_until_complete(self.is_drained(node)):
-                            self.deploy(node)
-                        else:
-                            msg += ", but is drained"
-                    else:
-                        msg += ", but is not on the allow list"
-                    print(msg)
-                    continue
-
-                node_time = node_time.replace(tzinfo=dt.timezone.utc)
-                if node not in self.stats.nodes:
-                    self.stats.nodes[node] = AutoDeployerNodeStats(last_seen=node_time)
+        try:
+            while not self._stop:
+                now = utcnow()
+                if self._last_update:
+                    elapsed = (now - self._last_update).total_seconds()
+                    if elapsed < self._sampling_interval_in_s:
+                        self.show()
+                        time.sleep(1)
+                        continue
                 else:
-                    self.stats.nodes[node].last_seen = node_time
+                    self._last_update = utcnow()
 
-                last_seen_in_s = (now - node_time).total_seconds()
-                msg = f"{node} last seen: {last_seen_in_s:10.1f} s ago"
-                if self.allow_list is None or node in self.allow_list:
-                    if last_seen_in_s > self._sampling_interval_in_s:
-                        if not loop.run_until_complete(self.is_drained(node)):
-                            msg = f"{msg} -- requires redeployment of probe"
-                            self.deploy(node)
+                now = utcnow()
+                self.messages = []
+                self.messages.append(f"-- autodeploy check: {now}")
+
+                last_probe_timestamp = None
+                if self.app_settings.db_schema_version == "v1":
+                    last_probe_timestamp = loop.run_until_complete(self.dbi.get_last_probe_timestamp())
+                else:
+                    if self.cluster_name is None:
+                        raise ValueError("Missing cluster_name")
+
+                    last_probe_timestamp = loop.run_until_complete(
+                            self.dbi.get_last_probe_timestamp(cluster=self.cluster_name)
+                    )
+                    logger.info(last_probe_timestamp)
+
+                expected_nodes = self.all_nodes()
+                for node in expected_nodes:
+                    node_time = last_probe_timestamp.get(node, None)
+                    if not node_time:
+                        msg = f"{node} has not been seen before"
+                        if self.allow_list is None or node in self.allow_list:
+                            msg += ", but is in the allow list"
+                            if not loop.run_until_complete(self.is_drained(node)):
+                                self.deploy(node)
+                            else:
+                                msg += ", but is drained"
                         else:
-                            msg = f"{msg} -- but node is drained"
-                print(msg)
+                            msg += ", but is not on the allow list"
+                        self.messages.append(msg)
+                        continue
 
-            self.save_stats()
-            start_time = utcnow()
+                    node_time = node_time.replace(tzinfo=dt.timezone.utc)
+                    if node not in self.stats.nodes:
+                        self.stats.nodes[node] = AutoDeployerNodeStats(last_seen=node_time)
+                    else:
+                        self.stats.nodes[node].last_seen = node_time
+
+                    last_seen_in_s = (now - node_time).total_seconds()
+                    msg = f"{node} last seen: {last_seen_in_s:10.1f} s ago"
+                    if self.allow_list is None or node in self.allow_list:
+                        if last_seen_in_s > self._sampling_interval_in_s:
+                            if not loop.run_until_complete(self.is_drained(node)):
+                                msg = f"{msg} -- requires redeployment of probe"
+                                self.deploy(node)
+                            else:
+                                msg = f"{msg} -- but node is drained"
+                    self.messages.append(msg)
+
+                self.save_stats()
+                self._last_update = utcnow()
+                self.show()
+        finally:
+            if self._screen:
+                self._screen.clear()
+                curses.echo()
+                curses.nocbreak()
+                curses.endwin()
 
 class AutoDeployerSonar(AutoDeployer):
     async def is_drained(self, node: str) -> bool:
@@ -168,8 +248,8 @@ class AutoDeployerSonar(AutoDeployer):
         if not node_states:
             return False
 
-        for state in node_states[0].states:
-            if state.startswith("drain"):
+        for state in node_states[0]['states']:
+            if state.lower().startswith("drain"):
                 return True
 
         return False
--- src/slurm_monitor/cli/autodeploy.py
+++ src/slurm_monitor/cli/autodeploy.py
@@ -1,11 +1,19 @@
 from argparse import ArgumentParser
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 from slurm_monitor.cli.base import BaseParser
 from slurm_monitor.autodeploy import AutoDeployer, AutoDeployerSonar
 from slurm_monitor.app_settings import AppSettings
+from slurm_monitor.config import (
+    SLURM_MONITOR_LOG_FORMAT,
+    SLURM_MONITOR_LOG_STYLE,
+    SLURM_MONITOR_LOG_DATE_FORMAT
+)
+
 
 logger = logging.getLogger(__name__)
+logger.propagate = False
 
 class AutoDeployParser(BaseParser):
     def __init__(self, parser: ArgumentParser):
@@ -27,6 +35,12 @@ class AutoDeployParser(BaseParser):
                 nargs='+',
                 default=None,
                 help="Provide the list of nodes for which redeployment is permitted"
+        )
+
+        parser.add_argument("--log-output",
+                type=str,
+                default=None,
+                help="Output file for the log - default: slurm-monitor.auto-deploy.<cluster-name>.log, disable logging to file by specifying 'none'",
         )
 
         parser.add_argument("--command",
@@ -53,9 +67,30 @@ class AutoDeployParser(BaseParser):
                     deploy_command=args.command,
                     allow_list=args.allow_list)
 
-        deployer.start()
+        log_output = args.log_output
+        # if log_output is None (the default), we set the default
+        # log file name
+        if log_output is None:
+            log_output = "slurm-monitor.auto-deploy.log"
+            if args.cluster_name:
+                log_output = "slurm-monitor.auto-deploy" + args.cluster_name + ".log"
+        elif log_output.lower() == 'none':
+            log_output = None
 
-        input("Press CTRL-C to stop")
+        if log_output:
+            formatter = logging.Formatter(
+                fmt=SLURM_MONITOR_LOG_FORMAT,
+                datefmt=SLURM_MONITOR_LOG_DATE_FORMAT,
+                style=SLURM_MONITOR_LOG_STYLE
+            )
+            root_logger = logging.getLogger()
+            root_logger.handlers.clear()
 
+            file_handler = TimedRotatingFileHandler(log_output, when='d', interval=3, backupCount=1)
+            file_handler.setLevel(logging.getLevelName(args.log_level))
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+            root_logger.addHandler(file_handler)
+
+        deployer.run()
         logger.info("Shutting down ...")
-        deployer.stop()
