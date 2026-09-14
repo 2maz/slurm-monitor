c9e3bc0a87bd0f39a73ec17d2b849dac293a3cc8
Edvard Aksnes <edvard.aksnes@gmail.com>
2026-09-14 14:51:45 +0200

    includes query listing jobs exceeding requested resources

--- src/slurm_monitor/db/v2/query.py
+++ src/slurm_monitor/db/v2/query.py
@@ -213,6 +213,101 @@ class PopularPartitionsByNumberOfJobs(Query):
     """
 
 
+class JobsExceedingRequestedResources(Query):
+    """
+    Generate a query to output:
+        partition, number_of_jobs (total), avg_time (per job)
+    """
+    @property
+    def statement(self):
+        cluster = self.ensure_parameter('cluster')
+
+        return fr"""
+        with job_requested_resources as (
+            select distinct on (job_id)
+                job_id,
+                "time",
+                coalesce((regexp_match(requested_resources, '\ynode=(\d+)'))[1]::int, 0) as nodes_requested,
+                coalesce((regexp_match(requested_resources, '\ycpu=(\d+)'))[1]::int, 0) as cpus_requested,
+                coalesce((regexp_match(requested_resources, '\ygpu=(\d+)'))[1]::int, 0) as gpus_requested,
+                coalesce(
+                    mem[1]::numeric * case upper(mem[2])
+                        when 'K' then 1.0/1024
+                        when 'M' then 1
+                        when 'G' then 1024
+                        when 'T' then 1024::numeric * 1024
+                        when 'P' then 1024::numeric * 1024 * 1024
+                        else 1
+                    end,
+                    0
+                ) as mem_requested_mib,
+                coalesce((regexp_match(requested_resources, '\ybilling=(\d+)'))[1]::int, 0) as billing,
+                requested_resources
+            from sample_slurm_job
+            left join lateral
+                regexp_match(requested_resources, '\ymem=(\d+(?:\.\d+)?)\s*([KMGTP])?', 'i')
+                as mem on true
+            where cluster = '{cluster}'
+            and "time" >= now() - interval '1 month'
+            and job_state = 'RUNNING'
+            and job_id not in (797193,797235,797236,797329,797330,797331)
+            order by job_id, "time" asc
+        ),
+        sum_data as (
+            select
+                sp.job,
+                r.cpus_requested,
+                r.mem_requested_mib,
+                r.gpus_requested,
+                sp."time",
+                sum(sp.resident_memory) as sum_resident_memory,
+                sum(sp.virtual_memory)  as sum_virtual_memory,
+                sum(sp.cpu_util)        as sum_cpu_util,
+                sum(sp.num_threads)     as sum_num_threads,
+                sum(sp.data_read)       as sum_data_read,
+                sum(sp.data_written)    as sum_data_written,
+                sum(sp.data_cancelled)  as sum_data_cancelled
+            from sample_process sp
+            join job_requested_resources r on sp.job = r.job_id
+            where sp.cluster = 'ex3.simula.no'
+            and sp."time" >= now() - interval '1 hour'
+            group by sp.job, r.cpus_requested, r.mem_requested_mib, r.gpus_requested, sp."time"
+        ),
+        aggregated_data as (
+            select
+                sum_data.job as job,
+                cpus_requested,
+                mem_requested_mib,
+                gpus_requested,
+                coalesce(count(distinct uuid), 0) as gpus_used,
+                count(*) as n_samples,
+                max(sum_resident_memory) as max_resident_memory,
+                avg(sum_resident_memory) as avg_resident_memory,
+                max(sum_virtual_memory)  as max_virtual_memory,
+                avg(sum_virtual_memory)  as avg_virtual_memory,
+                max(sum_cpu_util)        as max_cpu_util,
+                avg(sum_cpu_util)        as avg_cpu_util,
+                max(sum_num_threads)     as max_num_threads,
+                avg(sum_num_threads)     as avg_num_threads,
+                max(sum_data_read)       as max_data_read,
+                max(sum_data_written)    as max_data_written,
+                max(sum_data_cancelled)  as max_data_cancelled
+            from sum_data
+            left join sample_process_gpu on sum_data.job = sample_process_gpu.job
+            group by sum_data.job, cpus_requested, mem_requested_mib, gpus_requested
+            order by sum_data.job
+        )
+        select
+            job,
+            max_cpu_util > cpus_requested*1.2 as max_cpu_util_exceeded_requested,
+            avg_cpu_util > cpus_requested*1.5 as avg_cpu_util_exceeded_requested,
+            max_virtual_memory > mem_requested_mib as max_virtual_memory_exceeded_requested,
+            gpus_used > gpus_requested as gpus_used_exceeded_requested
+        from aggregated_data
+        order by job;
+    """
+
+
 
 class QueryMaker:
     db: Database
@@ -222,6 +317,7 @@ class QueryMaker:
             "user-success-job-results": UserSuccessJobResults,
             "user-failed-job-results": UserFailedJobResults,
             "popular-partitions-by-number-of-jobs": PopularPartitionsByNumberOfJobs,
+            "jobs-exceeding-resource-usage": JobsExceedingRequestedResources,
     }
 
     def __init__(self, db: Database):
